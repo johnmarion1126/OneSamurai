c1bd5baca3bbcaacf231073f2cb3186e2b7ca038
John Marion Sanchez <johnmarion1126@gmail.com>
2022-07-14 19:20:41 -0700

    Create enemy entity

--- src/scripts/Enemy.cs
+++ src/scripts/Enemy.cs
@@ -3,19 +3,16 @@ using System;
 
 public class Enemy : AnimatedSprite
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private SpriteManager animtedSprite;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        animtedSprite = new SpriteManager(this);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+  public void attack()
+  {
+    animtedSprite.setSpriteState("Attacking");
+    GD.Print("Enemy attacks");
+  }
 }
--- src/scripts/Main.cs
+++ src/scripts/Main.cs
@@ -4,8 +4,10 @@ using System;
 public class Main : Node
 {
     private Timer countdown;
+    private Timer enemyTimer;
 
     private Player player;
+    private Enemy enemy;
 
     public override void _Ready()
     {
@@ -13,15 +15,26 @@ public class Main : Node
         GD.Randomize();        
 
         player = GetNode<Player>("Player");
+        enemy = GetNode<Enemy>("Enemy");
 
         countdown = GetNode<Timer>("Countdown");
+        enemyTimer = GetNode<Timer>("EnemyTimer");
+
         countdown.WaitTime = 10.0f;
+        enemyTimer.WaitTime = 15.0f;
+
         countdown.Start();
+        enemyTimer.Start();
     }
 
     public void onCountdownTimeout()
     {
         GD.Print("DONE");
         player.isTimerRunning = false;
+    }
+
+    public void onEnemyTimerTimeout()
+    {
+        enemy.attack();
     }
 }
